f0c5e5065579fd6d654a2a0f5923635c669bf0bf
ARCADIO ALEXIS CALVILLO MADRID <arcadio.calvillo@itam.mx>
2019-09-24 20:39:26 -0500

    Tarea. Constelaciones de modulación

---
+++ TareaModulaciones/QAM16.m
@@ -0,0 +1,14 @@
+%Modulación 16 QAM 
+close all;
+N=16;
+k=log2(N);
+n=900000;
+xV=randi([0,1],1,n);
+dataInMatrix = reshape(xV,n/k,k);   % Reshape data into binary k-tuples, k = log2(M)
+simb = bi2de(dataInMatrix);            
+xMod=qammod(simb,16,'bin');
+xRec=awgn(xMod,0,'measured');
+s=scatterplot(xRec,1,0,'g+');   
+hold on;
+scatterplot(xMod,1,0,'ro',s),title('QAM16');
+
---
+++ TareaModulaciones/QPSK.m
@@ -0,0 +1,13 @@
+%Modulación QPSK
+close all;
+N=4;
+k=log2(N);
+n=900000;
+xV=randi([0,1],1,n);
+dataInMatrix = reshape(xV,n/k,k);   % Reshape data into binary k-tuples, k = log2(M)
+simb = bi2de(dataInMatrix);            
+xMod=pskmod(simb,N,pi/N);
+xRec=awgn(xMod,50,'measured');
+s=scatterplot(xRec,1,0,'g+');   
+hold on;
+scatterplot(xMod,1,0,'ro',s),title('QAM16');
